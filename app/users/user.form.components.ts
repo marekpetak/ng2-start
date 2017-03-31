428123eb2f646da6763df32d0a7f7a6a982006a6
Marek Petak <marekpetak@kannegiesser.com>
2017-03-31 12:52:45 +0100

    Add User add form

---
+++ app/users/user.form.components.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { User} from '../shared/models/user';
+
+@Component({
+    selector: 'user-form',
+    styles: [`
+        form {
+            padding: 10px;
+            background: gray;
+            border-radius: 3px;
+        }
+    `],
+    template: `
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" class="form-control" name="username" required>
+            </div>
+
+            <button type="submit" class="btn btn-lg btn-block btn-primary">
+                Create User
+            </button>
+        </form>
+    `
+})
+export class UserFormComponent {
+
+}
